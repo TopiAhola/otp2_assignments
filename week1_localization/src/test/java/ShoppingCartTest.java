301c4c8232f9846a7d8a7006162b285a52a085df
TopiAhola <aholatos@gmail.com>
2026-03-20 23:15:09 +0200

    week1 assignment in progress test added

--- week1_localization/src/test/java/ShoppingCartTest.java
+++ week1_localization/src/test/java/ShoppingCartTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -5,24 +7,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ShoppingCartTest {
 
-    @BeforeEach
-    void setUp() {
+    static ShoppingCart shoppingCart;
+
+    @BeforeAll
+    static void setUp() {
+        shoppingCart = new ShoppingCart();
+        shoppingCart.addToCart("A",5, 123.5);
+        shoppingCart.addToCart("B",5, 123.5);
     }
 
     @Test
     void addToCart() {
+        assertDoesNotThrow(() -> {
+            shoppingCart.addToCart("c",5, 123.5);
+            shoppingCart.addToCart("d",5, 123.5);
+        });
     }
 
     @Test
     void testToString() {
+        String a = """
+        A   5   123.5   617,50
+        B   5   123.5   617,50
+        """;
+        assertEquals(a, shoppingCart.toString());
     }
 
     @Test
     void getItemCount() {
+        assertEquals(20, shoppingCart.getItemCount());
     }
 
     @Test
     void getTotalCost() {
+        assertEquals(1235.0, shoppingCart.getTotalCost());
     }
 
 
