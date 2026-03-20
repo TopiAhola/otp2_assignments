ee1771f2d62b16e51836c6218d0f1cb40b324411
TopiAhola <aholatos@gmail.com>
2026-03-20 22:56:31 +0200

    week1 assignment in progress language translated

---
+++ week1_localization/src/test/java/ShoppingCartTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShoppingCartTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addToCart() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void getItemCount() {
+    }
+
+    @Test
+    void getTotalCost() {
+    }
+
+
+}
