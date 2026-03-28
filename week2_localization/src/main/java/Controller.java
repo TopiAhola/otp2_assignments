74f8c0a252c448b16c436b0c81e8d2a8689e172e
TopiAhola <aholatos@gmail.com>
2026-03-29 00:03:20 +0200

    week2 assignment in progress text direction added

--- week2_localization/src/main/java/Controller.java
+++ week2_localization/src/main/java/Controller.java
@@ -3,6 +3,7 @@ import java.util.ResourceBundle;
 
 
 import javafx.fxml.FXML;
+import javafx.geometry.NodeOrientation;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -70,6 +71,8 @@ public class Controller {
         //set default bundle
         bundles = new ResourceBundle[] {english,french,japanese, persian};
         currentBundle = english;
+        root.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+        btnIR.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
 
         //load text
         loadLanguageStrings();
@@ -77,22 +80,24 @@ public class Controller {
         //set functions for language buttons
         btnEN.setOnAction(e -> {
             currentBundle = english;
+            setLeftToRight();
             loadLanguageStrings();
         });
         btnFR.setOnAction(e -> {
             currentBundle = french;
+            setLeftToRight();
             loadLanguageStrings();
         });
         btnJP.setOnAction(e -> {
             currentBundle = japanese;
+            setLeftToRight();
             loadLanguageStrings();
         });
         btnIR.setOnAction(e -> {
             currentBundle = persian;
+            setRightToLeft();
             loadLanguageStrings();
         });
-
-
 
     }
 
@@ -119,10 +124,49 @@ public class Controller {
         txtPrice.setPromptText(currentBundle.getString("txtPrice"));
         lblResultText.setPromptText(currentBundle.getString("lblResultText"));
 
-
     }
 
 
+    protected void setLeftToRight(){
+
+        //persian is always rtl
+        btnIR.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+        btnEN.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+        btnFR.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+        btnJP.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+
+        titleLabel.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+        lblDistance.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+        lblConsumption.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+        lblPrice.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+        lblResult.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+        btnCalculate.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+        txtDistance.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+        txtConsumption.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+        txtPrice.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+        lblResultText.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+    }
+
+    protected void setRightToLeft(){
+
+        //persian is always rtl
+        btnIR.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+        btnEN.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+        btnFR.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+        btnJP.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+
+        titleLabel.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+        lblDistance.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+        lblConsumption.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+        lblPrice.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+        lblResult.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+        btnCalculate.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+        txtDistance.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+        txtConsumption.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+        txtPrice.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+        lblResultText.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+
+    }
 
 
 
